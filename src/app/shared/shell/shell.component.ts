3d8c94a3efe0bc958220947066c435211410d5c3
AustinTricquet <austintricquet@gmail.com>
2020-04-22 22:26:41 -0400

    Comments: -Added a Floating Aciton Button or FAB to the app shell -Added functionality to change icon and soon to change color on each tab change

--- src/app/shared/shell/shell.component.ts
+++ src/app/shared/shell/shell.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { BreakpointObserver, Breakpoints} from '@angular/cdk/layout';
-import { Observable } from 'rxjs';
+import { Observable, ArgumentOutOfRangeError } from 'rxjs';
 import { map, shareReplay} from 'rxjs/operators';
+import { MatTabChangeEvent } from '@angular/material';
 
 @Component({
   selector: 'app-shell',
@@ -10,6 +11,29 @@ import { map, shareReplay} from 'rxjs/operators';
   encapsulation: ViewEncapsulation.None
 })
 export class ShellComponent {
+
+  fabOptions = {
+    budget: {
+      color: "orange",
+      icon: "edit"
+    },
+    calendar: {
+      color: "red",
+      icon: "add"
+    },
+    goals: {
+      color: "blue",
+      icon: "add"
+    },
+    accounts: {
+      color: "green",
+      icon: "add"
+    }
+  }
+
+  fabColor = this.fabOptions.budget.color;
+  fabIcon = this.fabOptions.budget.icon;
+
 
   isHandset$: Observable<boolean> = this.breakpointObserver.observe([Breakpoints.Handset])
     .pipe(
@@ -20,6 +44,25 @@ export class ShellComponent {
 
   constructor(private breakpointObserver: BreakpointObserver) {}
 
+  tabChanged(tabChangeEvent: MatTabChangeEvent): void {
+    console.log('tabChangeEvent => ', tabChangeEvent);
+    console.log('index => ', tabChangeEvent.index);
+
+    if(tabChangeEvent.index == 0) {
+      this.fabColor = this.fabOptions.budget.color;
+      this.fabIcon = this.fabOptions.budget.icon;
+    } else if (tabChangeEvent.index == 1) {
+      this.fabColor = this.fabOptions.calendar.color;
+      this.fabIcon = this.fabOptions.calendar.icon;
+    } else if (tabChangeEvent.index == 2) {
+      this.fabColor = this.fabOptions.goals.color;
+      this.fabIcon = this.fabOptions.goals.icon;
+    } else {
+      this.fabColor = this.fabOptions.accounts.color;
+      this.fabIcon = this.fabOptions.accounts.icon;
+    }
+
+  }
   
 
 }
