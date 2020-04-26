cd3851edbe425c008970ac5dd3571783eb932d9f
AustinTricquet <austintricquet@gmail.com>
2020-04-25 20:33:21 -0400

    Comments: -Commented out the main feature sub router module so because at the moment it is not needed but will keep around in case the root route needs to be re routed to something else later on.

--- src/app/main-feature/main-feature-routing.module.ts
+++ src/app/main-feature/main-feature-routing.module.ts
@@ -1,15 +1,19 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import { TabsComponent } from './tabs/tabs.component';
+// Just found out that at the moment I do not need the router module for the 
+// main-feature module. But will keep just in case I need to re route the root '' to somehthing else later on.
 
 
-const routes: Routes = [
-    { path: '', component:TabsComponent}
-];
+//import { NgModule } from '@angular/core';
+//import { Routes, RouterModule } from '@angular/router';
+//import { TabsComponent } from './tabs/tabs.component';
 
-@NgModule({
-    imports: [RouterModule.forChild(routes)],
-    exports: [RouterModule]
-})
 
-export class MainFeatureRoutingModule { }
+//const routes: Routes = [
+   // { path: '', component:TabsComponent}
+//];
+
+//@NgModule({
+  //  imports: [RouterModule.forChild(routes)],
+  //  exports: [RouterModule]
+//})
+
+//export class MainFeatureRoutingModule { }
